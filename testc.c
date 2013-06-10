647d6bd60686a6932650724536671e0f4f679d81
Bryan Herman <bherman@mit.edu>
2013-06-10 17:34:26 -0400

    added creation, adding value, printing values and destroying vector routines

--- testc.c
+++ testc.c
@@ -8,63 +8,32 @@ struct Vec
 };
 
 extern "C" {
-    void testret_(float *mat);
-    float testdot_(int *N, float *vec1, float *vec2);
-    void testmat_(int *N, float *mat1, float *mat2, float *mat3);
-    void vecsetvalues_(Vec *x, int *n, double vals[]);
-    void vecgetvalues_(Vec *x, int *n);
+    void veccreate_(Vec *x, int *n);
+    void vecsetvalue_(Vec *x, int *i, double *val);
+    void vecprint_(Vec *x, int *n);
+    void vecdestroy_(Vec *x);
     }
 
-void testret_( float *mat ) 
-{
-    *mat = *mat + 1;
-}
-
-float testdot_(  int *N, float *vec1, float *vec2 ) 
-{
-    int i;
-    float dot;
-
-    dot = 0.0;
-
-    for (i=0; i<*N; i++) { 
-      dot = dot + *(vec1+i) * *(vec2+i);
-      }
-    return dot;
-}
-
-void testmat_(  int *N, float *mat1, float *mat2, float *mat3 ) 
-{
-    int i,j,k;
-    float dot;
-
-    // Assign stuff to mat3 from mat1 and mat2
-    for (i=0; i<*N; i++) { 
-      for (j=0; j<*N; j++) {
-         dot = 0.0;
-         for (k=0; k<*N; k++ ) {
-           dot = dot + *(mat1+(*N*j+k)) * *(mat2+(*N*i+j));
-         }
-         *(mat3+(*N*j+i)) = dot;
-       }
-    }
-
-}
-
-void vecsetvalues_( Vec *x, int *n, double vals[] )
+void veccreate_(Vec *x, int *n)
 {
    x -> values = Malloc(double *, *n);
-   for(int i = 0; i < *n; i++)
-   {
-      x -> values[i] = &vals[i];
-   }
 }
 
-void vecgetvalues_( Vec *x, int *n )
+void vecsetvalue_(Vec *x, int *i, double *val)
+{
+   x -> values[*i] = val;
+}
+
+void vecprint_(Vec *x, int *n)
 {
    for(int i = 0; i < *n; i++)
    {
      printf("%f\n", *x -> values[i]);
    }
 
+}
+
+void vecdestroy_(Vec *x)
+{
+   free(x -> values);
 }
--- testcode.f90
+++ testcode.f90
@@ -2,79 +2,16 @@ program testcode
 
 #define Vec integer(8)
 
-  integer veclen 
-  real mat, dot
-  real vec1(2), vec2(2)
-  real matrix1(3,3), matrix2(3,3), matrix3(3,3)
-  real(8) :: vec3(4) = (/1.0_8,2.0_8,3.0_8,4.0_8/)
   Vec :: x
   integer :: n = 4
+  integer :: i
+  real(8) :: vec(4) = (/1.0_8,2.0_8,3.0_8,4.0_8/)
 
-! interface
-!   function testdot(veclen, vec1, vec2)
-!     integer :: veclen
-!     real :: vec1(*)
-!     real :: vec2(*)
-!     real :: dot
-!   end function testdot
-
-!   subroutine vecsetvalues(x, n, vec)
-!     Vec :: x
-!     integer :: n
-!     real(8)        :: vec(*)
-!   end subroutine vecsetvalues
-! end interface
-
-  veclen = 2
-  matdim = 3
-
-  vec1(1) = 1.0
-  vec1(2) = 2.0
-  vec2(1) = 3.0
-  vec2(2) = 4.0 
-
-  matrix1(1,1) = 1.0
-  matrix1(2,1) = 0.0
-  matrix1(3,1) = 0.0
-  matrix1(1,2) = 0.0
-  matrix1(2,2) = 1.0
-  matrix1(3,2) = 0.0
-  matrix1(1,3) = 0.0
-  matrix1(2,3) = 0.0
-  matrix1(3,3) = 1.0
-
-  matrix2(1,1) = 1.0
-  matrix2(2,1) = 0.0
-  matrix2(3,1) = 0.0
-  matrix2(1,2) = 0.0
-  matrix2(2,2) = 2.0
-  matrix2(3,2) = 0.0
-  matrix2(1,3) = 0.0
-  matrix2(2,3) = 0.0
-  matrix2(3,3) = 3.0
-
-  mat = 0.0;
-
-! mat should get incremented by 1 in testret 
-  do i = 1, 10
-     call testret(mat)
+  call veccreate(x, n)
+  do i = 1, n
+    call vecsetvalue(x, i-1, vec(i))
   end do
-
-! testdot should return the dot product of vec1 and vec2 as a real
-  dot = testdot(veclen, vec1, vec2)
-
-! testmat uses matrix1 and matrix2 to calculate matrix3
-  call testmat(matdim, matrix1, matrix2, matrix3)
-
-  print *, mat, '  ', dot
-  print *, 
-  do i = 1, matdim
-    write (6,*) (matrix3(j,i), j=1,matdim)
-  end do
-
-! set values to vector
-  print *, n
-  call vecsetvalues(x, n, vec3)
-  call vecgetvalues(x, n)
+  call vecprint(x, n)
+  call vecdestroy(x)
 
 end program testcode
